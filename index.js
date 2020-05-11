4c63028c710f87e47ddb994c05199fc29e0bd5cb
Tan Boon Kiat <boonkiat.tan@ifastfinancial.com>
2020-05-11 23:21:08 +0800

    added sdk button

--- index.js
+++ index.js
@@ -2,10 +2,7 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
 import {TestNavigator} from 'testapp/src/app/navigator/TestNavigator';
 
 export const SdkButton = TestNavigator;
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
